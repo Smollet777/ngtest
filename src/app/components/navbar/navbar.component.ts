ad505fba88fbbbbda2b3fae9e74e3317c3cacfa2
Ivan Smollet <smollet777@bigmir.net>
2019-02-04 15:27:53 +0200

    upgrade angular & material & material navbar to v7

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -10,7 +11,10 @@ import { Observable } from 'rxjs';
 })
 export class NavbarComponent {
 
-  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
 
   constructor(private breakpointObserver: BreakpointObserver) { }
 
